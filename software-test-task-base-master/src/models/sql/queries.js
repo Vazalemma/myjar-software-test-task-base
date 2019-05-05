b5aa359a85db63b296d2a89d76e7dff3425398d6
heouna <heouna@ttu.ee>
2019-05-05 16:41:38 +0300

    TECH-91 Test task complete gen3 rev1

---
+++ software-test-task-base-master/src/models/sql/queries.js
@@ -0,0 +1,16 @@
+const { QueryFile } = require('pg-promise');
+const path = require('path');
+
+const clients = {
+	get: new QueryFile(path.join(__dirname, './clients/get.sql')),
+	getOne: new QueryFile(path.join(__dirname, './clients/getOne.sql')),
+	deleteOne: new QueryFile(path.join(__dirname, './clients/deleteOne.sql')),
+	createOne: new QueryFile(path.join(__dirname, './clients/createOne.sql')),
+	updateOne: new QueryFile(path.join(__dirname, './clients/updateOne.sql')),
+};
+
+const queries = {
+	clients,
+};
+
+module.exports = queries;
